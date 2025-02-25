2a2af83ff6cf171ab761a7a67e53ee03dd3a9bf0
MrFlava <thatelitemaili33t@gmail.com>
2025-02-25 22:31:17 +0200

    Work on structure and debugging

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ def get_soup(url: str) -> BeautifulSoup:
 def find_cards(soup: BeautifulSoup) -> ResultSet:
     return soup.find_all('div', attrs={'class': 'card thumbnail'})
 
-def parse_single_card(card, item_type: str):
-    return  {
-            'price': float(card.find('h4', class_='price').text.replace('$', '')),
-            'title': card.find('a', class_='title').get('title'),
-            'description': card.find('p', class_='description').text,
-            'reviews_count': int(card.find('p', class_='review-count').text.replace('reviews', '')),
-            'type': item_type,
-    }
+def parse_category(cards: ResultSet, item_type: str):
+    for card in cards:
+        return  {
+                'price': float(card.find('h4', class_='price').text.replace('$', '')),
+                'title': card.find('a', class_='title').get('title'),
+                'description': card.find('p', class_='description').text,
+                'reviews_count': int(card.find('p', class_='review-count').text.replace('reviews', '')),
+                'type': item_type,
+        }
 
 
 def mine_items():
@@ -34,11 +35,14 @@ def mine_items():
     for category, url in PRODUCT_SOURCES_URLS.items():
         soup = get_soup(url)
         cards = find_cards(soup)
-        
+
+        items.append(parse_category(cards, category))
+
 
 
 def main():
-    pass
+    items = mine_items()
+    print(items)
 
 if __name__ == '__main__':
     main()
