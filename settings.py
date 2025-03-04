2c6f0642b128a957b7843dc06ed4db8d3943ca34
MrFlava <thatelitemaili33t@gmail.com>
2025-03-04 20:43:34 +0200

    Provide db settings

--- settings.py
+++ settings.py
@@ -1,2 +1,10 @@
 import os
 from dotenv import load_dotenv
+
+load_dotenv()
+
+# db settings
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_COLLECTION = os.getenv("DB_COLLECTION")
