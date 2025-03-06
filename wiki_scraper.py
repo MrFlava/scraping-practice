018a10f2a4b5b0b3daf1b444ef3e4b85a31a0d7b
MrFlava <thatelitemaili33t@gmail.com>
2025-03-06 21:50:37 +0200

    Add url for future scraping

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -1,2 +1,7 @@
 import  requests
 from bs4 import BeautifulSoup
+
+# Needs to scrap all urls of the performers or members of band (including band name)
+
+WIKI_ROCK_HALL_OF_FAME = 'https://en.wikipedia.org/wiki/List_of_Rock_and_Roll_Hall_of_Fame_inductees'
+
